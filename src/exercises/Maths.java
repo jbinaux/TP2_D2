bfe3cba7a9e921795997f8d2cc1f6a23df0aa156
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-07-12 15:28:00 +0200

    first commit

---
+++ src/exercises/Maths.java
@@ -0,0 +1,23 @@
+package exercises;
+
+public class Maths {
+
+	public static int x, y;
+	public static double w;
+	
+	public static void loveMath()
+	{
+		System.out.println("I love maths");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		x = 1;
+		y = 3;
+		w = 2.35;
+		
+		System.out.println("x = " + x + "\ny = " + y + "\nw = " + w);
+		loveMath();
+	}
+
+}
